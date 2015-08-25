257186e514320632d78c0fccc70c177f74042643
Ankit Gordhandas <AnkitAthos@Ankits-MacBook-Pro.local>
2015-08-24 20:16:22 -0700

    First Commit

--- outputIdxOfCoding.py
+++ outputIdxOfCoding.py
@@ -5,17 +5,18 @@ import variables
 mirror = variables.exons
 idx = []
 counter = 1
+exonIdx = 0
 
 for i in range(len(variables.gene)):
     letter = variables.gene[i]
     if letter.isupper():
-        if mirror[0].isupper():
+        if mirror[exonIdx].isupper():
             idx.append(counter)
             counter = counter + 1
-            mirror = mirror[1:]
+            exonIdx = exonIdx + 1
         else:
             idx.append ('nan')
-            mirror = mirror[1:]
+            exonIdx = exonIdx
     else:
         idx.append ('nan')
 
